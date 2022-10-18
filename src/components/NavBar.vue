ca7d99f7aa0c3e093c164d37e80fc8e35a19c3cc
darrendolosa <70249885+darrendolosa@users.noreply.github.com>
2022-10-18 18:18:43 +0800

    First commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav>
+      <v-toolbar app>
+       <v-app-bar-nav-icon @click='toggleDrawer'></v-app-bar-nav-icon>
+      <v-toolbar-title >
+        <span class="font-weight-light"> </span>
+        <span></span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+
+
+
+
+      </v-toolbar>
+
+     <v-navigation-drawer app v-model="drawer" >
+       <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          router 
+          :to="item.path" :prepend-icon="item.icon">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+
+        </v-list-item>
+
+     </v-navigation-drawer>
+  </nav>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import vuetify from '../plugins/vuetify';
+
+  const drawer = ref(false)
+
+  const  items =  ref([
+
+          { title: 'Dashboard', icon: 'mdi-mouse', path: '/dashboard' },
+           { title: 'Laboratory 1', icon: 'mdi-mouse', path: '/selfone' },
+           { title: 'Laboratory 2', icon: 'mdi-monitor', path: '/programming' },
+           { title: 'Laboratory 3', icon: 'mdi-calculator', path: '/about' },
+           { title: 'Research', icon: 'mdi-ab-testing', path: '/selftwo' },
+           { title: 'laboratory 4', icon: 'mdi-ab-testing', path: '/labfour' },
+           { title: 'laboratory 5', icon: 'mdi-ab-testing', path: '/axios' },
+        ])
+
+ function toggleDrawer(){
+  return drawer.value = !drawer.value
+ }
+ 
+ 
+</script>
